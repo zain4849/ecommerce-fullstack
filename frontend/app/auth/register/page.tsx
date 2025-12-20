e7726a3b167810f0b93ec86436f0aa4211c52ad4
Zain Ali <s.zaink5@gmail.com>
2025-12-20 09:19:36 +0400

    Implement form validation in Register component using react-hook-form and zod

--- frontend/app/auth/register/page.tsx
+++ frontend/app/auth/register/page.tsx
@@ -11,20 +11,49 @@ import Image from "next/image";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "@/store/store";
 import { register } from "@/store/authSlice";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+
+const registerSchema = z
+  .object({
+    name: z.string().min(1, "Name is required"),
+    email: z.email(),
+    password: z.string().min(8, "Password must be atleast 8 characters"),
+    confirmedPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmedPassword, {
+    message: "Passwords do not match",
+    path: ["confirmedPassword"],
+  });
+
+type RegisterFormData = z.infer<typeof registerSchema>;
+// // use above instead of:
+// type FormData = {
+//   email: string,
+//   password: string,
+// }
+// which is w/out zod and takes more code and manual entry to get form to work
 
 export default function Register() {
-  const dispatch = useDispatch<AppDispatch>()
+  const dispatch = useDispatch<AppDispatch>();
   // const loading = useAuthStore((s) => s.loading);
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
+  // const [error, setError] = useState("");
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setError(""); // clear already displayed error
+  const {
+    register: registerField,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmit = async (data: RegisterFormData) => {
+    console.log(data)
+
     try {
-      await dispatch(register({name, email, password})).unwrap();
+      // await dispatch(register({ name, email, password })).unwrap();
+      await dispatch(register({ name: data.name, email: data.email, password: data.password })).unwrap();
       window.location.href = "/";
     } catch (error) {
       const err = error as AxiosError<{ error: string }>;
@@ -35,28 +64,39 @@ export default function Register() {
   return (
     <div>
       <h1 className="text-[3.125rem] font-bold mb-8 text-center">Sign Up</h1>
-      <form onSubmit={handleSubmit} className="space-y-7">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-7">
         <Input
           className="pl-7 py-7 rounded-2xl placeholder:text-muted"
           type="name"
           placeholder="Name"
-          value={name}
-          onChange={(e) => setEmail(e.target.value)}
+          // value={name}
+          // onChange={(e) => setEmail(e.target.value)}
+          {...registerField("name")}
+
+
         />
+          {errors.name && <p className="text-red-500">{errors.name.message}</p>}
+
         <Input
           className="pl-7 py-7 rounded-2xl placeholder:text-muted"
-          type="email"
+          // type="email"
           placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
+          // value={email}
+          // onChange={(e) => setEmail(e.target.value)}
+          {...registerField("email")}
         />
+          {errors.email && <p className="text-red-500">{errors.email.message}</p>}
+
         <Input
           className="pl-7 py-7 rounded-2xl placeholder:text-muted"
-          type="password"
+          // type="password"
           placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
+          // value={password}
+          // onChange={(e) => setPassword(e.target.value)}
+          {...registerField("password")}
         />
+          {errors.password && <p className="text-red-500">{errors.password.message}</p>}
+
         <Button type="submit" className="w-full py-7 rounded-2xl">
           Sign Up
         </Button>
@@ -71,7 +111,6 @@ export default function Register() {
             height={25}
             alt="Login with Google"
           />
-
         </div>
         <p className="text-center text-foreground font-[50]">
           Already have an account?{" "}
