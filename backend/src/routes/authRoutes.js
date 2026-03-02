4791f191d603d0991e712e321e83eb00c40897da
Zain Ali <s.zaink5@gmail.com>
2026-03-02 11:26:50 +0400

    Modify .env.example for consistency; remove console log from authRoutes

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -4,8 +4,6 @@ import { register, login } from "../controllers/authController.js"
 
 const router = express.Router()
 
-console.log('From authRoutes:', process.env.MONGO_URI)
-
 router.post("/register", register)
 router.post("/login",login)
 
